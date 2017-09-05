9b632394859d97afdd3b92d6bee23e175ce5caaf
Tanapong Saramon <tanapong@ashitagroup.com>
2017-09-05 09:46:53 +0700

    fix

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var MyServer = require('./myServer/myServer')
 var sv = new MyServer(3000);
 sv.initApp();
-app.getApp().get('/api/webhook', (req, res) => {
+sv.getApp().get('/api/webhook', (req, res) => {
     if (req.query['hub.mode'] === 'subscribe' &&
         req.query['hub.verify_token'] === '112233445566xxccvvbb') {
         console.log('Validating webhook')
@@ -11,7 +11,7 @@ app.getApp().get('/api/webhook', (req, res) => {
         res.sendStatus(403)
     }
 })
-app.getApp().post('/api/webhook', (req, res) => {
+sv.getApp().post('/api/webhook', (req, res) => {
     var data = req.body
     console.log(data);
     io.emit('chat', data)
