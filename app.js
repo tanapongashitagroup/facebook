622dab2c566f6b0a3c91b424f27f7ad981152a47
Tanapong Saramon <tanapong@ashitagroup.com>
2017-09-05 10:22:50 +0700

    123

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ sv.getApp().get('/api/webhook', (req, res) => {
 sv.getApp().post('/api/webhook', (req, res) => {
     var data = req.body
     console.log(data);
-    //io.emit('chat', data)
+    res.sendStatus(200)
+        //io.emit('chat', data)
 });
 sv.getApp().get('/', (req, res) => {
     res.json({ message: 'api working' });
